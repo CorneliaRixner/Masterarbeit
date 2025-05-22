081bbe7720eaa9f21bb7898a0282cf432959182f
CorneliaRixner <72449026+CorneliaRixner@users.noreply.github.com>
2025-05-22 21:19:10 +0200

    Update overwatch2_entfernen_irrelevanter_elemente.py

--- overwatch2_entfernen_irrelevanter_elemente.py
+++ overwatch2_entfernen_irrelevanter_elemente.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import re
 
-file_name = r"C:\Users\conri\Documents\FIT Master\5. Semester\Masterarbeit\03_Datensätze\Reddit-Beiträge\Overwatch2\overwatch2_reddit_data_final_vorbereitet.csv"
+file_name = r"C:\Users\conri\Documents\FIT Master\5. Semester\Masterarbeit\03_Datensätze\Reddit-Beiträge\Overwatch2\overwatch2_reddit_data_update_perks.csv"
+
+# CSV laden
 df = pd.read_csv(file_name)
 
 # Emoji Bereich U+1F600 - U+1F64F
@@ -25,14 +27,17 @@ def clean_text(text):
 
     return text
 
-# Bereinigen
+# Bereinigen der relevanten Spalten
 df['body_clean'] = df['body'].apply(clean_text)
 df['title_clean'] = df['title'].apply(clean_text)
 
-# Vorschau
-print(df[['title_clean', 'body_clean']].head())
+# Vorschau der bereinigten Daten
+print(df[['created', 'title_clean', 'body_clean']].head())
 
-# Speichern
-df.to_csv(r"C:\Users\conri\Documents\FIT Master\5. Semester\Masterarbeit\03_Datensätze\Reddit-Beiträge\Overwatch2\overwatch2_reddit_data_cleaned.csv", index=False)
-print("Datei gespeichert.")
+# Speichern der bereinigten Datei mit der ersten Spalte als Datum
+output_file = r"C:\Users\conri\Documents\FIT Master\5. Semester\Masterarbeit\03_Datensätze\Reddit-Beiträge\Overwatch2\overwatch2_reddit_data_cleaned_update_perks_with_date.csv"
+df[['created', 'title_clean', 'body_clean']].to_csv(output_file, index=False)
+print("Bereinigte Datei gespeichert unter:", output_file)
+
+
 
